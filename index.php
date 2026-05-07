b2930064e3766ee9e165c6c7975455932b2f8a3f
wajahattunio86-dot <wajahattunio86@gmail.com>
2026-05-07 16:11:25 +0500

    index.html

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+include("navbar.php");
+
+
+
+include("db.php");
+
+
+
+
+?>
+
+
+<p id="word1">Shop the Future of Style</p>
+<p id="word2">Discover handpicked collections designed to elevate your everyday look. Quality products, unbeatable prices, and fast delivery all in one place.</p>
+<button id="btn1">learn more</button>
+
+
+
+<!-- <div id="container">
+<div id="card_container">
+    <img src="g2.webp" alt="">
+</div>
+</div> -->
+
+
+
+
+    <?php
+include("footer.php");
+    ?>
+     
+
+  
